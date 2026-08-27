355328e08b82790dca8ab056929227e55446059a
Kiana Kaslana <130233273+JiangKaslana@users.noreply.github.com>
2026-08-27 11:35:04 +0800

    feat: register launcher detection UI

--- frontend/src/app-modules.ts
+++ frontend/src/app-modules.ts
@@ -10,6 +10,7 @@ import { prefetchStatsWorker } from "./backend/browser-adapter.ts";
 import { initI18n } from "./core/i18n/locale.ts";
 import { friendlyError } from "./utils/dom/errors.ts";
 import { checkUpdateSilent } from "./features/version-updater.ts";
+import { registerLauncherDetection } from "./features/launcher-detection.ts";
 import { applyUIPrefs } from "./views/app-content/settings/ui-prefs.ts";
 import { loadView } from "./utils/module-loader.ts";
 import { revealMainWindow } from "./startup-reveal.ts";
@@ -103,6 +104,7 @@ export { normalizeTheme, applyTheme, initTheme };
   setTimeout(() => prefetchStatsWorker(), 2000);
  } finally {
    await appContentReady;
+   registerLauncherDetection();
    await revealMainWindow(() => Window.Show());
  }
 })();
